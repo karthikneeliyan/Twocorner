850ba2f5a3aeabeaf15775d9a6482a780115a8e9
Selvaraj <selva@thworks.org>
2022-02-26 09:13:36 +0530

    Add login profile sections

---
+++ src/App/App.tsx
@@ -0,0 +1,53 @@
+import './App.css';
+
+import React, { useEffect, useState } from 'react';
+
+import { Login } from '../Login';
+import { Profile } from '../Profile/Profile';
+import { Auth } from '../types';
+import logo from './2c_logo.svg';
+
+const LS_KEY = 'login:auth';
+
+interface State {
+	auth?: Auth;
+}
+
+export const App = (): JSX.Element => {
+	const [state, setState] = useState<State>({});
+
+	useEffect(() => {
+		// Access token is stored in localstorage
+		const ls = window.localStorage.getItem(LS_KEY);
+		const auth = ls && JSON.parse(ls);
+		setState({ auth });
+	}, []);
+
+	const handleLoggedIn = (auth: Auth) => {
+		localStorage.setItem(LS_KEY, JSON.stringify(auth));
+		setState({ auth });
+	};
+
+	const handleLoggedOut = () => {
+		localStorage.removeItem(LS_KEY);
+		setState({ auth: undefined });
+	};
+
+	const { auth } = state;
+
+	return (
+		<div className="App">
+			<header className="">
+				<img src={logo} className="App-logo" alt="logo" />
+				<div className="Twocorners">Twocorners</div>
+			</header>
+			<div className="App-intro">
+				{auth ? (
+					<Profile auth={auth} onLoggedOut={handleLoggedOut} />
+				) : (
+					<Login onLoggedIn={handleLoggedIn} />
+				)}
+			</div>
+		</div>
+	);
+};
---
+++ src/Login/Login.tsx
@@ -0,0 +1,133 @@
+import './Login.css';
+
+import React, { useState } from 'react';
+import Web3 from 'web3';
+declare var window: any;
+
+import { Auth } from '../types';
+
+interface Props {
+	onLoggedIn: (auth: Auth) => void;
+}
+
+let web3: Web3 | undefined = undefined; // Will hold the web3 instance
+
+export const Login = ({ onLoggedIn }: Props): JSX.Element => {
+	const [loading, setLoading] = useState(false); // Loading button state
+
+	const handleAuthenticate = ({
+		publicAddress,
+		signature,
+	}: {
+		publicAddress: string;
+		signature: string;
+	}) =>
+		fetch(`${process.env.REACT_APP_BACKEND_URL}/auth`, {
+			body: JSON.stringify({ publicAddress, signature }),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			method: 'POST',
+		}).then((response) => response.json());
+
+	const handleSignMessage = async ({
+		publicAddress,
+		nonce,
+	}: {
+		publicAddress: string;
+		nonce: string;
+	}) => {
+		try {
+			console.log("publicAddress",publicAddress);
+			web3 = new Web3(window.ethereum);
+			const signature = await web3.eth.personal.sign(
+				`I am signing my one-time nonce: ${nonce}`,
+				publicAddress,
+				'' // MetaMask will ignore the password argument here
+			);
+			console.log("publicAddress",publicAddress);
+			console.log("signature",signature);
+			return { publicAddress, signature };
+		} catch (err) {
+			throw new Error(
+				'You need to sign the message to be able to log in.'
+			);
+		}
+	};
+
+	const handleSignup = (publicAddress: string) =>
+		fetch(`${process.env.REACT_APP_BACKEND_URL}/users`, {
+			body: JSON.stringify({ publicAddress }),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			method: 'POST',
+		}).then((response) => response.json());
+
+	const handleClick = async () => {
+		// Check if MetaMask is installed
+		if (!window.ethereum) {
+			window.alert('Please install MetaMask first.');
+			return;
+		}
+
+		if (!web3) {
+			try {
+				// Request account access if needed
+				await window.ethereum.enable();
+
+				// We don't know window.web3 version, so we use our own instance of Web3
+				// with the injected provider given by MetaMask
+				web3 = new Web3(window.ethereum);
+			} catch (error) {
+				window.alert('You need to allow MetaMask.');
+				return;
+			}
+		}
+
+		const coinbase = await web3.eth.getCoinbase();
+		if (!coinbase) {
+			window.alert('Please activate MetaMask first.');
+			return;
+		}
+
+		const publicAddress = coinbase.toLowerCase();
+		setLoading(true);
+
+		// Look if user with current publicAddress is already present on backend
+		fetch(
+			`${process.env.REACT_APP_BACKEND_URL}/users?publicAddress=${publicAddress}`
+		)
+			.then((response) => response.json())
+			// If yes, retrieve it. If no, create it.
+			.then((users) =>
+				users.length ? users[0] : handleSignup(publicAddress)
+			)
+			// Popup MetaMask confirmation modal to sign message
+			.then(handleSignMessage)
+			// Send signature to backend on the /auth route
+			.then(handleAuthenticate)
+			// Pass accessToken back to parent component (to save it in localStorage)
+			.then(onLoggedIn)
+			.catch((err) => {
+				window.alert(err);
+				setLoading(false);
+			});
+	};
+
+	return (
+		<div>
+			<p className='Private-Spaces-Uniq'>
+			    Private Spaces, Unique Conversations
+			</p>
+			<div>
+			<p className='Sign-in'>
+			    Sign-in
+			</p>
+			<button className="Rectangle-4" onClick={handleClick}>
+				{loading ? 'Loading...' : 'Login with MetaMask'}
+			</button>
+			</div>
+		</div>
+	);
+};
